cab4fceaccf3c4e45111a4e2d2f200354136c82a
Alikin E.A <alikin@proitr.ru>
2018-03-10 19:35:39 +0500

    first commit

---
+++ service1/src/main/java/Application.java
@@ -0,0 +1,32 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Alikin E.A. on 10.03.18.
+ */
+@RestController
+@EnableAutoConfiguration
+@Configuration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "service1";
+    }
+
+    @GetMapping("/health-check")
+    public ResponseEntity<String> myCustomCheck() {
+        String message = "Testing my healh check function";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+}
